ab0ae18ba3d4bf465894dbab0184bc7e7d1ab4c2
Juan Pablo Nores <noresjpn@gmail.com>
2025-02-27 12:39:33 -0300

    remove unnecessary import

--- src/app/usePageContext.tsx
+++ src/app/usePageContext.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useEffect, useRef, useState } from "react";
-import { deflate } from "zlib";
 
 const calculateBoxes = () => {
   const boxSize = window.innerWidth / 100;
