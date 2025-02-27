b0474cfa9f1aa84e9aaee207c8d56f2f27c30932
Juan Pablo Nores <noresjpn@gmail.com>
2025-02-27 15:29:41 -0300

    favicon and app name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,10 @@ const BoxesGrid: React.FC = () => {
 
   return (
     <div className="page__grid">
+      <link rel="shortcut icon" href="/icon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       {contextMenu && (
         <div
           className="page__context-menu"
