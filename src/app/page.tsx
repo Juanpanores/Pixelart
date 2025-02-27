96dc85f2839b29fd44d67b15da9bd68e36757e47
Juan Pablo Nores <noresjpn@gmail.com>
2025-02-27 12:35:33 -0300

    Pixelart first draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,67 @@
-import Image from "next/image";
+"use client"
+import React from "react";
+import "./page.styles.css";
+import { usePageContext } from "./usePageContext";
 
-export default function Home() {
+const BoxesGrid: React.FC = () => {
+  const {
+    COLORS,
+    handleMouseDown,
+    handleMouseEnter,
+    handleRightClick,
+    handleMenu,
+    contextMenu,
+    boxesState,
+    boxesRef,
+    handleMouseUp,
+    isVisible,
+    hanldeMenuClose,
+    defaultColor
+  } = usePageContext();
+
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="page__grid">
+      {contextMenu && (
+        <div
+          className="page__context-menu"
+          style={{
+            top: contextMenu.y -5,
+            left: contextMenu.x -5,
+            opacity: isVisible ? 1 : 0,
+            transform: isVisible ? "scale(1)" : "scale(0.9)",
+            transition: "opacity 0.3s ease-in-out, transform 0.3s ease-in-out",
+            }}
+          onContextMenu={(e) => {e.preventDefault()}}
+          onMouseLeave={hanldeMenuClose}
+        >
+          {COLORS.map((color, index) => (
+            <div
+              key={index}
+              className="page__context-menu__option "
+              style={{ "--color": color, border: color === defaultColor ? "3px solid black": "1px solid black" } as React.CSSProperties}
+              onClick={() => handleMenu(index)}
+              data-color
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          ))}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+      {boxesState.current.map((_, index) => (
+        <div
+          key={index}
+          ref={(el) => {
+            if (el) boxesRef.current[index] = el;
+          }}
+          className="page__box"
+          onMouseUp={handleMouseUp}
+          onMouseDown={(event) => handleMouseDown(event,index)}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onContextMenu={(event) => handleRightClick(event)}
+        />
+      ))}
     </div>
   );
-}
+};
+
+export default BoxesGrid;
---
+++ src/app/usePageContext.tsx
@@ -0,0 +1,109 @@
+"use client"
+import React, { useEffect, useRef, useState } from "react";
+import { deflate } from "zlib";
+
+const calculateBoxes = () => {
+  const boxSize = window.innerWidth / 100;
+  const height = window.innerHeight;
+  const rows = Math.floor(height / boxSize);
+  const totalBoxes = 100 * rows;
+  return new Array(totalBoxes).fill(null).map(() => ({ color: "white", active: false }));
+};
+
+const COLORS = ["red", "blue", "green", "yellow", "purple", "black", "gray", "orange"];
+
+const usePageContext = () => {
+  const [contextMenu, setContextMenu] = useState<{ x: number; y: number } | null>(null);
+  const [defaultColor, setDefaultColor] = useState(COLORS[0]);
+  const [isVisible,setIsVisible] = useState(false)
+  const boxesRef = useRef<HTMLDivElement[]>([]);
+  const boxesState = useRef(calculateBoxes());
+  const isSelecting = useRef(false);
+  const needsUpdate = useRef(false);
+  
+  
+  const updateUI = () => {
+    if (!needsUpdate.current) return;
+    needsUpdate.current = false;
+    boxesRef.current.forEach((box, index) => {
+      if (box) {
+        box.style.backgroundColor = boxesState.current[index].color;
+      }
+    });
+  };
+
+  const toggleColor = (index: number) => {
+    if (boxesState.current[index]) {
+      
+        boxesState.current[index].active = true;
+        boxesState.current[index].color = defaultColor;
+        needsUpdate.current = true;
+
+      requestAnimationFrame(updateUI);
+    }
+  };
+
+  const handleMouseDown = (event: React.MouseEvent, index: number) => {
+    if (event.button !== 0) return;
+    hanldeMenuClose()
+    isSelecting.current = true;
+    if(boxesState.current[index].active === true && boxesState.current[index].color === defaultColor)
+      {
+        boxesState.current[index].color = "white";
+        boxesState.current[index].active = false;
+        needsUpdate.current = true;
+        requestAnimationFrame(updateUI);
+      }
+      else toggleColor(index);
+    
+  };
+
+  const handleMouseUp = () => {
+    isSelecting.current = false;
+  };
+
+  const handleMouseEnter = (index: number) => {
+    if (isSelecting.current) {
+      toggleColor(index);
+    }
+  };
+
+  const handleRightClick = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setContextMenu({ x: event.clientX, y: event.clientY });
+    setTimeout(() => setIsVisible(true), 10);
+  };
+
+  const handleMenu = (index: number) => {
+    setDefaultColor(COLORS[index]);
+    hanldeMenuClose()
+  };
+
+  const hanldeMenuClose = () => {
+    setIsVisible(false)
+    setTimeout(() => setContextMenu(null), 10);
+  }
+
+  useEffect(() => {
+    boxesState.current = calculateBoxes();
+    needsUpdate.current = true;
+    requestAnimationFrame(updateUI);
+  }, []);
+
+  return {
+    COLORS,
+    handleMouseDown,
+    handleMouseEnter,
+    handleRightClick,
+    handleMenu,
+    contextMenu,
+    boxesState,
+    boxesRef,
+    handleMouseUp,
+    isVisible,
+    hanldeMenuClose,
+    defaultColor
+  }
+}
+
+export { usePageContext };
